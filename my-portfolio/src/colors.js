b2430ebe45e94893e6774197b78b1b2a4b1c45cf
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-12-05 12:07:59 +0330

    remove extra files

---
+++ my-portfolio/src/colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    lightTeal: '#b2d8d8', // RGB: (178,216,216)
+    mediumTeal: '#66b2b2', // RGB: (102,178,178)
+    teal: '#008080',       // RGB: (0,128,128)
+    darkTeal: '#006666',   // RGB: (0,102,102)
+    darkestTeal: '#004c4c' // RGB: (0,76,76)
+  };
+  
+  export default COLORS;
+  
